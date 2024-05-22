2bd83a359a3a76f9191debd8f828e7dfb0a0a18b
Pranav Bodawala <b.pranav.k@gmail.com>
2024-05-22 22:40:51 +0000

    refactor: :recycle: Configure test_main as a class

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,19 +5,21 @@ from linkit2.main import app
 client = TestClient(app)
 
 
-def test_read_main():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"message": "Hello World"}
+class TestMain:
+    def setup_class(self):
+        self.client = TestClient(app)
 
+    def test_read_main(self):
+        response = self.client.get("/")
+        assert response.status_code == 200
+        assert response.json() == {"message": "Hello World"}
 
-def test_check_health_live():
-    response = client.get("/health/live")
-    assert response.status_code == 200
-    assert response.json() == {"status": "LIVE"}
+    def test_check_health_live(self):
+        response = self.client.get("/health/live")
+        assert response.status_code == 200
+        assert response.json() == {"status": "LIVE"}
 
-
-def test_check_health_db():
-    response = client.get("/health/db")
-    assert response.status_code == 200
-    assert response.json() == {"db": "CONNECTED"}
+    def test_check_health_db(self):
+        response = self.client.get("/health/db")
+        assert response.status_code == 200
+        assert response.json() == {"db": "CONNECTED"}
